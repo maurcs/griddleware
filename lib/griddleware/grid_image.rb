157c644da7fabb88cb5db59821d93300542d3cb9
Marcus Rosentrater <conradmr@gmail.com>
2010-03-10 17:43:18 -0500

    Don't force types. Let Griddle::Attachment force the types

--- lib/griddleware/grid_image.rb
+++ lib/griddleware/grid_image.rb
@@ -8,17 +8,16 @@ module Griddleware
     def call(env)
       rack = Rack::Request.new(env)
       if rack.path =~ /^\/grid\/([\w]+)\/([\w]+)\/(.+)$/
-        owner_type, owner_id = [$1.classify, $2.to_i]
+        owner_type, owner_id = [$1.classify, $2]
         style_path = $3.split('/')
         file_name = style_path.pop
         name = style_path.join("/")
         record = {
-          :name => name.to_sym,
+          :name => name,
           :owner_type => owner_type,
           :owner_id => owner_id
         }
         attachment = Griddle::Attachment.attachment_for(record)
-        puts attachment.exists?
         [200, {"Content-Type" => attachment.content_type}, attachment.file.read]
       else
         @app.call(env)
